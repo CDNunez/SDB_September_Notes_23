511b59209c36877b4e6e3999b33c064ed71090df
CDNunez <carlosnunez114@gmail.com>
2023-10-10 20:59:22 -0400

    Added Functions and Parameters Notes

---
+++ Unit_two/Functions/functions.js
@@ -0,0 +1,138 @@
+/* 
+*   Functions
+
+    Blocks of code that create small programs. They are used for repetitive tasks. When user does x, program does y. Functions are built to reduce repeated code - functional programming.
+
+    -Block of code that performs a particular task.
+    -simplified programs that run a task when invoked.
+
+    !Function Declaration Structure
+    (1)        (2)
+    function hi() {
+        console.log("Hi");
+    }
+
+    1) Keyword
+    2)Name of the function
+        -parenthesis is for parameters
+*/
+
+//Declarative Functions
+function hi() {
+    console.log('Hi!');
+}
+
+/* 
+*    Function Expression
+    (1)
+    let hey = function() {
+        console.log('Hi!');
+    }
+
+    1)The variable "hey" is now representative of the function "hi"
+
+    !Does not get hoisted
+*/
+
+let hey = function hello() {
+    console.log('Hello World!');
+}
+
+hey(); // Hello World!
+
+
+//Invoking - means to run a function
+
+/* 
+*    Invoking
+
+    function hi() {
+        console.log('hi!);
+    }
+
+    (1)
+    hi();
+
+    1)Invoking function 'hi' -- write the name of the function followed by a parenthesis.
+*/
+
+function greetings() {
+    console.log("Hello");
+}
+
+greetings; //shouldnt log
+greetings(); //logs Hello
+console.log(greetings); //logs [Function: greetings]
+
+// Create a function that when invoked lists out the numbers 1 through 10. function name "counting"
+
+function counting() {
+    for( let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+counting();
+
+// Given an array, create a function that lists out the values individually.
+
+let array = ['This', 'is', 'really', 'lame'];
+
+function arrList() {
+    for(i = 0; i <= 3; i++) {
+        console.log(array[i]);
+    }
+}
+
+arrList();
+
+let arrayList = function arrayList() {
+    for(item of array) {
+        console.log(item);
+    }
+}
+
+arrayList();
+
+let dogArray = [
+    "Husky",
+    "Shih Tzu",
+    "Corgi",
+    "Catahoula",
+    "Shiba",
+    "Golden Retriever"
+];
+
+//for of loop - calls the value in array
+
+let dogList = function doglist() {
+    for(dog of dogArray) {
+        console.log(dog);
+    }
+}
+
+dogList();
+
+//or for loop
+
+console.log("------------");
+
+let pupList = function pupList() {
+    for(i = 0; i <= 5; i++) {
+        console.log(dogArray[i]);
+    }
+}
+
+pupList();
+
+//or for in loop - calls the index of the array
+
+console.log("---------");
+
+let petList = function petList() {
+    for(dog in dogArray) {
+        console.log(dogArray[dog]);
+    }
+}
+
+petList();
---
+++ Unit_two/Functions/parameters.js
@@ -0,0 +1,40 @@
+/* 
+*   Parameters & Arguments
+
+    -Parameters are placeholder data to be passed through the function
+    -Can be named anything but should be appropriate
+
+    Structure example
+                    (1)
+        function hi(name) {
+            console.log(`Hi, ${name}`);
+        }                       (2)
+            (3)
+        hi("Joe");
+
+        1)Parameter(s) that the function is accepting or place holding.
+        2)Using string interpolation, we can refer to the parameter that was given to the function.
+        3)This is where we define what the parameter's value will be.
+            -Known as an argument
+*/
+
+function order(ticket) {
+    console.log(`I exchanged my ticket for a ${ticket}!`);
+}
+
+order('cheese pizza');
+order('burger and fries');
+order('cheesecake');
+
+function myName (firstName, lastName) {
+    console.log(`My name is ${firstName} ${lastName}.`);
+}
+
+myName('Carlos', 'Nunez');
+
+function yourName( fName, lName) {
+    let fullName = fName + " " + lName;
+    console.log(`Hello, my name is ${fullName}.`);
+}
+
+yourName('Rock', 'Lee');
