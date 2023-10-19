d468bd921460d8fc85f362bc6c0e934218d8fc93
CDNunez <carlosnunez114@gmail.com>
2023-10-18 20:55:27 -0400

    Added notes to iterationMethods.js

--- Unit_three/Arrays/iterationMethods.js
+++ Unit_three/Arrays/iterationMethods.js
@@ -86,3 +86,116 @@ const takeFive = myNumberArray.filter(myNumberArray => {
     return result; //stores the value of result to takeFive
 })
 console.log(takeFive); //filters out 5
+
+//---------------notes continued---------------
+
+let movies = [
+    { name: "Top Gun 2", category: "Action"},
+    { name: "Scary Movie 5", category: "Horror"},
+];
+
+movies.push({name: "It", category: "Horror"});
+movies.push({name: "Get Out", category: "Horror"});
+movies.push({name: "Taken", category: "Action"});
+
+const actionMovies = movies.filter( movie => movie.category === 'Action'); //filters out for action movies
+const horrorMovies = movies.filter(movie => movie.category === "Horror"); // filters out for horror movies
+
+console.log("Horror :", horrorMovies);
+console.log("Action: ", actionMovies);
+
+//! .forEach()
+/* 
+    it provides a callback function for each of the elements..?
+*/
+
+let newFoodList = [
+    'apple', 'pear','mushroom','cheese','peach'
+];
+
+for(let i = 0; i < newFoodList.length; i++) {
+    console.log(newFoodList[i]);// logs each item in the array
+}
+
+//newFoodList.forEach(item => console.log("For Each: ", item)); //logs same as above loop
+
+newFoodList.forEach((item, index) => {
+    console.log("For Each: ", item, "index: ", index);
+})// logs the index of each item inside the array
+
+/*
+    !Challenge
+    - (Go look at MDN docs to remind you):
+    - Create an array containing movies titles
+    - Use .forEach() to list your movies
+    - Add another movie at the end //push
+    - And replace one of your existing movies with another one //splice
+*/
+
+let movieArray = ["Pokemon", "Yu Gi Oh", "DragonBall Z"];
+
+movieArray.push("One Piece");
+
+movieArray.splice(1, 1, "BayBlade");
+
+movieArray.forEach(movie => console.log(movie));
+
+//! .find()
+
+let tmnt= [
+    'Mikey', 'Donnie', 'Leo', 'Raph', 'Splinter', 'Shredder', 'Baxter'
+];
+
+////tmnt.forEach( (character, index) => console.log("Character: ", character,"length of item: ", character.length, "Index of Character: ", index));
+
+let character = "Leo";
+console.log("Find: ", tmnt.find(c => c == character)); //searches the array for the parameter/data we specify by cycling through the array
+
+character = "Shredder";//updating the variable further down wont affect the first result due to how js is read and how initializing and redefining values work
+console.log("Find: ", tmnt.find(c => c == character)); 
+
+character = "Splinter";
+tmnt.find((c, i) => console.log("Index: ", c == character, "index: ", i));
+
+//! .map()
+//cycle through an array of items and adjust for what is needed
+
+
+let numberArray = [];
+let fizzBuzzArray = [];
+
+for(let i = 0; i < 101; i++) numberArray.push(i);//logs numbers from 0 to 101 inside of numberArray --immediate return for the loop
+
+numberArray.map(x => {
+    if(x % 15 === 0) fizzBuzzArray.push(x); //logs numbers inside numberArray that are divisible by 15 into fizzBuzzArray
+});
+
+console.log(fizzBuzzArray);
+
+/*
+ - First check if you are working with an array
+    - Using a method, flip the values within the array 
+    (what was in index 4 is now in 0, 3 to 1, etc.)
+    - Using a method, print the values of the newly arranged array
+    
+    hint: 
+       - search something like "arrays", "javascript", "reverse"
+       - look in your notes to see how we can check if something is an array
+*/
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log(typeof arr);
+
+/*if( arr instanceof Array === true) {
+    let revArr = arr.reverse();
+    revArr.forEach(num => console.log(num));
+} else {
+    console.log("not an array");
+}*/
+
+console.log(arr instanceof Array === true);
+
+let backwards = arr.reverse();
+
+console.log(backwards);
